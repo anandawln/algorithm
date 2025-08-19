bcc69dd88303f5c2e3a2f636d579990cb851144a
Wulan <anandawulan127@gmail.com>
2025-08-19 20:59:08 +0700

    Create type_data.cpp

---
+++ type_data.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int angkaBulat = 10;
+    float angkaKoma = 1.5f;
+    char karakter = 'a';
+    char string [100] = "Hello";
+
+    printf("%d\n", angkaBulat);
+    printf("%.2f\n", angkaBulat);
+    printf("%c\n", angkaBulat);
+    printf("%s\n", angkaBulat);
+
+        return 0;
+}
